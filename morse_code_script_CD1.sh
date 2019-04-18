c6bbf285d8ffb2b52634a111eb1bd362ebf17e0d
cdecesaris <sharpsabresword@g.ucla.edu>
2019-04-18 16:46:38 -0700

    morse code assignments added

--- morse_code_script_CD1.sh
+++ morse_code_script_CD1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #script designed to translate English letters into morse code
 #would be like sh ___.dh morse.txt
-
+#takes the name stored in $name and runs through each letter then grabs the line of morse code present.
 name="E M I L Y"
 
 for i in $name
--- morse_code_script_CD2.sh
+++ morse_code_script_CD2.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+#script designed to translate English letters into morse code
+#would be like sh ___.dh morse.txt
+#takes the name stored in $name and runs through each letter then grabs the morse pattern on that matches the letter
+#use of printf gives an output that is all on the same line in accordance to the challenge
 
 
-
-name="E M I L Y"
+name="C H R I S T I N A"
 
 for i in $name
 do translate=$( grep -w  $i morse.txt )
